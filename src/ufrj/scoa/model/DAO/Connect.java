bfff7d9f43129c040d431f2d4aeff3772f59b899
Emerson Yamamoto <emersonyamamoto@gmail.com>
2015-04-23 18:36:39 -0300

    Removendo main do Connect

--- src/ufrj/scoa/model/DAO/Connect.java
+++ src/ufrj/scoa/model/DAO/Connect.java
@@ -5,35 +5,38 @@ import java.sql.DriverManager;
 
 public class Connect {
 	
-	 public static void main (String[] args)
-	    {
-	        Connection conn = null;
-
-	        try
-	        {
-	            String userName = "root";
-	            String password = "";
-	            String url = "jdbc:mysql://localhost/scoa";
-	            Class.forName ("com.mysql.jdbc.Driver").newInstance();
-	            conn = DriverManager.getConnection (url, userName, password);
-	            System.out.println ("Conexão com o BD estabelecida!");
-	        }
-	        catch (Exception e)
-	        {
-	            System.err.println ("Não foi possível estabelecer conexão com o BD");
-	        }
-	        finally
-	        {
-	            if (conn != null)
-	            {
-	                try
-	                {
-	                    conn.close ();
-	                    System.out.println ("Conexão finalizada");
-	                }
-	                catch (Exception e) { /* ignore close errors */ }
-	            }
-	        }
-	    }
-
+   public boolean connectBD() {
+	   
+	   Connection conn = null;
+	   
+        try
+        {
+            String userName = "root";
+            String password = "";
+            String url = "jdbc:mysql://localhost/scoa";
+            Class.forName ("com.mysql.jdbc.Driver").newInstance();
+            conn = DriverManager.getConnection (url, userName, password);
+            System.out.println ("Conexão com o BD estabelecida!");
+            return true;
+        }
+        catch (Exception e)
+        {
+            System.err.println ("Não foi possível estabelecer conexão com o BD");
+            return false;
+        }
+        finally
+        {
+            if (conn != null)
+            {
+                try
+                {
+                    conn.close ();
+                    System.out.println ("Conexão finalizada");
+                }
+                catch (Exception e) { /* ignore close errors */ }
+            }
+        }
+        
+   }
+        
 }
