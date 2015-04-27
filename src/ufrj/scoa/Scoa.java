a1531cb3961ad8104ba94b4e071c4f115a705522
Emerson Yamamoto <emersonyamamoto@gmail.com>
2015-04-27 01:57:17 -0300

    Tela de boas vindas

--- src/ufrj/scoa/Scoa.java
+++ src/ufrj/scoa/Scoa.java
@@ -5,17 +5,21 @@ import ufrj.scoa.model.DAO.CourseDAO;
 import ufrj.scoa.model.VO.Course;
 import ufrj.scoa.view.CourseCreationView;
 import ufrj.scoa.view.StudentCreationView;
+import ufrj.scoa.view.WelcomeView;
 
 public class Scoa {
 
 	public static void main(String[] args) {
 		
-		StudentCreationView studentCreationView = new StudentCreationView();
+		/*StudentCreationView studentCreationView = new StudentCreationView();
 		CourseCreationView courseCreationView = new CourseCreationView();
 		
         new CourseController(courseCreationView);
         
-        courseCreationView.setVisible(true);
+        courseCreationView.setVisible(true);*/
+		
+		WelcomeView welcomeView = new WelcomeView();
+		welcomeView.setVisible(true);
 
 	}
 
